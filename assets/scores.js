5cbd46e9c4527efdb23dbdbfd629fefc562e536e
Brian Murphy <ktp200@gmail.com>
2022-11-17 18:11:43 -0500

    score page gets sorted, added history clear button

--- assets/scores.js
+++ assets/scores.js
@@ -1,6 +1,12 @@
 function showScores() {
   // get any existing saved scores
   var savedScores = JSON.parse(window.localStorage.getItem("scores")) || [];
+
+  // sort saved scores by score
+  savedScores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
   // put scores on the lists
   for (var i = 0; i < savedScores.length; i += 1) {
     // create li tag for each high score
@@ -13,5 +19,12 @@ function showScores() {
   }
   }
 
+//clear history of scores and refresh the page
+function scoreHistory() {
+    window.localStorage.removeItem("scores");
+    window.location.reload();
+}
+
 //show scores
 showScores();
+document.getElementById("clear").onclick = scoreHistory;
